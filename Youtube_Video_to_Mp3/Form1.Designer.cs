39f6cc6a481ef3f675c387a7bb27a5b2f91fe478
bmont434 <beauharrison9@protonmail.com>
2021-03-18 22:44:36 -0400

    Upload from VS

---
+++ Youtube_Video_to_Mp3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Youtube_Video_to_Mp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblIndicator = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.Location = new System.Drawing.Point(330, 262);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(141, 46);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblIndicator
+            // 
+            this.lblIndicator.AutoSize = true;
+            this.lblIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIndicator.Location = new System.Drawing.Point(369, 103);
+            this.lblIndicator.Name = "lblIndicator";
+            this.lblIndicator.Size = new System.Drawing.Size(53, 25);
+            this.lblIndicator.TabIndex = 1;
+            this.lblIndicator.Text = "Start";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.Location = new System.Drawing.Point(94, 170);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(609, 30);
+            this.txtURL.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lblIndicator);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.TextBox txtURL;
+    }
+}
+
---
+++ Youtube_Video_to_Mp3/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediaToolkit;
+using VideoLibrary;
+using System.IO;
+using MediaToolkit.Model;
+
+namespace Youtube_Video_to_Mp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string SaveVideoToDisk(string link)
+        {
+            lblIndicator.Text = "In Progress";
+            var youTube = YouTube.Default; // starting point for YouTube actions
+            var video = youTube.GetVideo(link); // gets a Video object with info about the video
+            File.WriteAllBytes(@"C:\" + video.FullName, video.GetBytes());
+            return @"C:\" + video.FullName;
+        }
+
+        private void ConvertMp3(string filelocation)
+        {
+            var inputFile = new MediaFile { Filename = filelocation };
+            var outputFile = new MediaFile { Filename = filelocation + ".mp3" };
+
+            using (var engine = new Engine())
+            {
+                engine.Convert(inputFile, outputFile);
+            }
+            lblIndicator.Text = "Done!";
+        }
+    }
+}
+}
